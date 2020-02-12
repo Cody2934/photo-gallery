33029653064ceae004b3061593c9d7a005f0c4d6
Cody Brown <cody6lefever@gmail.com>
2020-02-12 08:28:38 -0800

    laying basics

---
+++ photo-gallery/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header.js";
+
+
+function App() {
+  return (
+  <div>
+
+      <Header/>
+      <div>
+      </div>
+
+      {/* ImageList with dropdown
+      <div>
+      </div> */}
+
+      {/* ActualImages that show monsters
+      <div>
+      </div> */}
+
+      
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  </div>
+  );
+}
+
+export default App;
---
+++ photo-gallery/src/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                <img src="https://imgix.bustle.com/rehost/2016/9/13/52e2a893-1b56-4101-814c-0c0238062b4a.png"/>
+                <h1>AHHHHHH! REAL MONSTERS</h1>
+            </header>
+        )}};
